131e9bf3df523686920ce1c119da153ac47c7a1e
Gowtham <58394964+gowthamthenarasu@users.noreply.github.com>
2023-12-06 14:20:53 +0530

    Update mine.py

--- mine.py
+++ mine.py
@@ -8,7 +8,7 @@ from langchain.chains.question_answering import load_qa_chain
 from langchain.llms import OpenAI
 
 # Set API keys
-os.environ["OPENAI_API_KEY"] = "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""
+os.environ["OPENAI_API_KEY"] = "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"
 os.environ["SERPAPI_API_KEY"] = "28c2445d1bfe7530595be6fbc858b1968d776af69c6034aa5feda50deab4b990"
 
 # PDF processing
